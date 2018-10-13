bab1789b8bff7f0760579aa3e440f380a5356e39
Andrei Nitu <an16228@my.bristol.ac.uk>
2018-10-14 00:17:08 +0100

    pad matrix with 0s, remove branches

--- stencil4.c
+++ stencil4.c
@@ -9,7 +9,6 @@
  * - change the timed forloop to go to 2 * niters and use tmp_row
  * - change stencil function to use tmp_row, also save current cell and reset it
  *   in tmp_row to use for the next cell to the right
- * v4 :
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,8 +17,8 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const int nx, const int ny, double * restrict image, double * restrict tmp_row);
-void init_image(const int nx, const int ny, double * image, double * tmp_row);
+void stencil(const int nx, const int ny, double *  image, double *  tmp_row);
+void init_image(const int nx, const int ny, double *  image, double *  rmp_row);
 void output_image(const char * file_name, const int nx, const int ny, double *image);
 double wtime(void);
 
@@ -37,16 +36,16 @@ int main(int argc, char *argv[]) {
   int niters = atoi(argv[3]);
 
   // Allocate the image
-  double *image = malloc( sizeof(double) * (nx+2) * (ny+2) );
-  double *tmp_row = malloc( sizeof(double) * (nx+2) * (ny+2) );
+  double *image = malloc(sizeof(double)*(nx+2)*(ny+2));
+  double *tmp_row = malloc(sizeof(double)*(nx+2));
 
   // Set the input image
-  init_image(nx, ny, image, tmp_row);
+  init_image(nx+2, ny+2, image, tmp_row);
 
   // Call the stencil kernel
   double tic = wtime();
   for (int t = 0; t < 2 * niters; ++t) {
-    stencil(nx, ny, image, tmp_row);
+    stencil(nx+2, ny+2, image, tmp_row);
   }
   double toc = wtime();
 
@@ -56,49 +55,53 @@ int main(int argc, char *argv[]) {
   printf(" runtime: %lf s\n", toc-tic);
   printf("------------------------------------\n");
 
-  output_image(OUTPUT_FILE, nx, ny, image);
+  output_image(OUTPUT_FILE, nx+2, ny+2, image);
   free(image);
 }
 
-void stencil(const int nx, const int ny, double * restrict image, double * restrict tmp_row) {
+void stencil(const int nx, const int ny, double *  image, double *  tmp_row) {
   double current_cell;
-  for( int i = 1; i < nx+1; i++ ) {
-    for( int j = 1; j < ny+1; j++ ) {
+  for( int i = 1; i < nx-1; i++ ) {
+    for( int j = 1; j < ny-1; j++ ) {
       current_cell = image[ j + i * ny ];
       image[ j + i * ny ] *= 0.6;
-      if (i > 0)    image[ j + i * ny ] += tmp_row[ j ] * 0.1;
-      if (i < nx-1) image[ j + i * ny ] += image[ j + (i + 1) * ny ] * 0.1;
-      if (j > 0)    image[ j + i * ny ] += tmp_row[ j - 1  ] * 0.1;
-      if (j < ny-1) image[ j + i * ny ] += image[ j + 1 + i * ny ] * 0.1;
+      image[ j + i * ny ] += tmp_row[ j ] * 0.1;
+      image[ j + i * ny ] += image[ j + (i + 1) * ny ] * 0.1;
+      image[ j + i * ny ] += tmp_row[ j - 1  ] * 0.1;
+      image[ j + i * ny ] += image[ j + 1 + i * ny ] * 0.1;
       tmp_row[ j ] = current_cell;
     }
   }
+  // Reset temporary row to 0s
+  for (int i = 0; i < ny; ++i)
+    tmp_row[i] = 0.0;
 }
 
 // Create the input image
 void init_image(const int nx, const int ny, double * image, double * tmp_row) {
   // Zero everything
-  for (int j = 0; j < ny+2; ++j) {
-    for (int i = 0; i < nx+2; ++i) {
+  for (int j = 0; j < ny; ++j) {
+    for (int i = 0; i < nx; ++i) {
       image[j+i*ny] = 0.0;
     }
   }
 
-  // Initialize temporary row
-  for (int i = 0; i < ny+2; ++i)
+  // Initialize temporary row with 0s
+  for (int i = 0; i < ny; ++i)
     tmp_row[i] = 0.0;
 
-  // Checkerboard
+  // Checkerboard --- margins = 0
   for (int j = 0; j < 8; ++j) {
     for (int i = 0; i < 8; ++i) {
-      for (int jj = j*ny/8; jj < (j+1)*ny/8; ++jj) {
-        for (int ii = i*nx/8; ii < (i+1)*nx/8; ++ii) {
+      for (int jj = j*(ny-2)/8; jj < (j+1)*(ny-2)/8; ++jj) {
+        for (int ii = i*(nx-2)/8; ii < (i+1)*(nx-2)/8; ++ii) {
           if ((i+j)%2)
-            image[ (jj + 1) + (ii + 1) * ny ] = 100.0;
+            image[(jj+1)+(ii+1)*ny] = 100.0;
         }
       }
     }
   }
+
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
@@ -112,22 +115,22 @@ void output_image(const char * file_name, const int nx, const int ny, double *im
   }
 
   // Ouptut image header
-  fprintf(fp, "P5 %d %d 255\n", nx, ny);
+  fprintf(fp, "P5 %d %d 255\n", nx-2, ny-2);
 
   // Calculate maximum value of image
   // This is used to rescale the values
   // to a range of 0-255 for output
   double maximum = 0.0;
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  for (int j = 1; j < ny-1; ++j) {
+    for (int i = 1; i < nx-1; ++i) {
       if (image[j+i*ny] > maximum)
         maximum = image[j+i*ny];
     }
   }
 
   // Output image, converting to numbers 0-255
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  for (int j = 1; j < ny-1; ++j) {
+    for (int i = 1; i < nx-1; ++i) {
       fputc((char)(255.0*image[j+i*ny]/maximum), fp);
     }
   }
